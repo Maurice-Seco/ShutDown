80473ad08708322ade25e335c36a282f198d7a15
Maurice-Seco <maurice.fehlemann99@gmail.com>
2025-09-15 04:35:50 +0200

    Add project files.

---
+++ ShutDown/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+Console.WriteLine("Bitte gebe eine stunden anzahl an"); 
+int stunden = Convert.ToInt32(Console.ReadLine()); 
+int stundenZuSekunden = stunden * 60 * 60; 
+Console.WriteLine("Jetzt die Minuten. (Falls du nur stunden haben möchtest geb hier 0 ein)"); 
+int minuten = Convert.ToInt32(Console.ReadLine()); 
+int minutenZuSekunden = minuten * 60;
+int sekunden = minutenZuSekunden + stundenZuSekunden;
+
+string argumente = $"/s /t {sekunden}";
+Process.Start("C:\\Windows\\System32\\shutdown.exe", argumente);
+Console.WriteLine($"PC fährt in bald herunter...");
+
